1f8da86f0d8bc1ca47b61daa8145d77631e04d05
miko3333 <kolrum69@gmail.com>
2019-02-01 12:47:12 +0300

    Fixed MemoryError

--- download_csv.py
+++ download_csv.py
@@ -1,3 +1,21 @@
+import os
+import string
+import json
+from datetime import datetime, timedelta
+import time
+import random
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
+from clickhouse_driver.client import Client
+
+from pandas_summary import DataFrameSummary
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from IPython.display import display
+
+from sklearn import metrics
+
 ch_fields = [
 'SessionId',
 'CampaignId',
@@ -41,95 +59,10 @@ ch_fields = [
 'Gender',
 'SspId']
 
-
-dima_fields = [
-"AdId",
-"AdvertisedAppId",
-"AdvertiserCategories",
-"AppCategory",
-"AppId",
-"Browser",
-"Device",
-"DeviceType",
-"AdvertiserId",
-"VisitorDayOfWeek",
-"OperatingSystemName",
-"PageDomain",
-"PageDomainAdvertiserId",
-"PlacementCategories",
-"PlacementPosition",
-"PlacementSize",
-"ScreenHeight",
-"ScreenSize",
-"ScreenWidth",
-"ServerHour",
-"Ssp",
-"VisitorLanguage",
-"VisitorRemarketingSegments"
-]
-
-
-fields = [
-'SessionId',
-'CampaignId',
-'BannerId',
-'AdWidth',
-'AdHeight',
-'VisitorId',
-'ThirdPartyVisitorId',
-'AdvertiserId',
-'LandingDomain',
-'BannerType',
-'ConnectionType',
-'PlacementCategories',
-'PlacementViewability',
-'TimezoneOffset',
-'ScreenWidth',
-'ScreenHeight',
-'AppCategory',
-'Language',
-'PublisherId',
-'SiteId',
-'PlacementId',
-'Domain',
-'AppId',
-'DeviceType',
-'DeviceCarrier',
-'DeviceModel',
-'Os',
-'OsVersion',
-'Browser',
-'BrowserVersion',
-'Country',
-'Region',
-'Sity',
-'Gender',
-'SspId']
-
-import os
-import string
-import json
-from datetime import datetime, timedelta
-import time
-import random
-
-import numpy as np
-import pandas as pd
-from pandas import DataFrame, Series
-from clickhouse_driver.client import Client
-
-from pandas_summary import DataFrameSummary
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from IPython.display import display
-
-from sklearn import metrics
-
-
-
-days_to_train = 14
-hours_to_train = 0
-days_to_test = 2
-hours_to_test = 0
+days_to_train = 0
+hours_to_train = 1
+days_to_test = 0
+hours_to_test = 1
 
 
 test_time_str = '2018-12-10 00:00:00'
@@ -151,7 +84,6 @@ print(f"""impressions:\nfrom  {train_datetime_from}\nuntil {train_datetime_till}
 clicks:\nfrom  {train_datetime_from}\nuntil {train_datetime_till_clicks}""")
 
 
-
 # дату кликов смотрим дальше, чтобы догнать позние клики
 clickhouse_query_template_clicks = """
 SELECT
@@ -169,29 +101,23 @@ PREWHERE (Date >= '{1}') AND (Date < '{2}') AND (ImpressionsCount = 1) AND (Sess
 FORMAT CSV
 """
 
-
-
-
-%%time
 clickhouse_hosts = ['db101', 'db102', 'db103', 'db104', 'db105']
+settings = {'max_block_size': 100000}
 
 cc = Client(random.choice(clickhouse_hosts), compression='lz4')
 
-views_train = cc.execute(clickhouse_query_template_views.format(', '.join(ch_fields), train_datetime_from, train_datetime_till))
-clicks_train = cc.execute(clickhouse_query_template_clicks.format(train_datetime_from, train_datetime_till_clicks))
+views_train = cc.execute_iter(clickhouse_query_template_views.format(', '.join(ch_fields), train_datetime_from, train_datetime_till), settings=settings)
+clicks_train = cc.execute_iter(clickhouse_query_template_clicks.format(train_datetime_from, train_datetime_till_clicks), settings=settings)
 
-views_test = cc.execute(clickhouse_query_template_views.format(', '.join(ch_fields), test_datetime_from, test_datetime_till))
-clicks_test = cc.execute(clickhouse_query_template_clicks.format(test_datetime_from, test_datetime_till_clicks))
+views_test = cc.execute_iter(clickhouse_query_template_views.format(', '.join(ch_fields), test_datetime_from, test_datetime_till), settings=settings)
+clicks_test = cc.execute_iter(clickhouse_query_template_clicks.format(test_datetime_from, test_datetime_till_clicks), settings=settings)
 
 cc.disconnect()
-
 
 
 len(views_train), len(views_test)
 if views_train < views_test:
     views_train, views_test = views_test, views_train
-
-
 
 
 def make_dataframe(views_data, click_data):
@@ -222,62 +148,16 @@ def df_features(data):
     data.loc[:,'UserHour'] = data['UserDate'].dt.hour
     data.loc[:,'UserDayOfWeek'] = data['UserDate'].dt.dayofweek
     data.drop(['Browser', 'BrowserVersion', 'Os', 'OsVersion', 'Date', 'Os',
-               'OsVersion', 'AdWidth', 'AdHeight', 'ScreenWidth', 'ScreenHeight',
-               'DeviceCarrier', 'DeviceModel', 'TimezoneOffset', 'Date', 'UserDate'], axis=1, inplace=True)
+    'OsVersion', 'AdWidth', 'AdHeight', 'ScreenWidth', 'ScreenHeight',
+    'DeviceCarrier', 'DeviceModel', 'TimezoneOffset', 'Date', 'UserDate'], axis=1, inplace=True)
     cols = list(data.columns.values)
     cols.pop(cols.index('click'))
     data = data[cols[1:]+['click']]
     return data
 
+
 raw_train_df = df_features(raw_train_df)
 raw_test_df = df_features(raw_test_df)
 
-
-def pd_to_format(format_name, input_df, name):
-    pd.options.mode.chained_assignment = None
-
-    input_df.replace('https://', '', inplace = True)
-    input_df.replace('http://', '', inplace = True)
-    input_df.replace(':', '.', inplace = True)
-
-    if format_name == 'vw':
-        input_df.replace({'click': {0: -1}}, inplace=True)
-        input_df.replace('', '__null__', inplace=True)
-
-        input_df.loc[:,'VW'] = input_df['click'].astype(str)
-
-        for i, j in zip(list(input_df.columns), list(string.ascii_letters)):
-            if i == 'click':
-                pass
-            else:
-                input_df['VW'] += ' |' + j + ' ' + input_df.loc[:,i].astype(str)
-
-        with open('click-dumps/' + name + '.vw', 'a') as tfile:
-            tfile.write(input_df['VW'].to_string(header=False, index=False))
-            tfile.close()
-
-        return True
-
-    if format_name == 'json': #разобраться с неймспейсами
-
-        data = input_df.rename(columns={'click':'_label'})
-        data.replace({'_label': {0: -1}}, inplace=True)
-        out = data.to_json(orient='records')
-
-        with open('click-dumps/' + name + '.json', 'w') as outfile:
-            json.dump(out, outfile)
-
-        return True
-
-    if format_name == 'csv':
-        input_df.to_csv('click-dumps/' + name + '.csv', index=False)
-
-        return True
-
-    else:
-        print('Wrong format')
-
-
-
-pd_to_format('csv', raw_train_df, 'train')
-pd_to_format('csv', raw_test_df, 'test')
+raw_train_df.to_csv('/click-dumps/train.csv')
+raw_test_df.to_csv('/click-dumps/test.csv')
