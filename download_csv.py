7431f6bfc9be08d2c00e19ff25c57c621bc243c4
miko3333 <kolrum69@gmail.com>
2019-02-01 16:22:36 +0300

    Timestamp error fixed

--- download_csv.py
+++ download_csv.py
@@ -103,7 +103,7 @@ def iter_download_from_clickhouse_to_csv(time_str, days_to_train, days_to_test):
 
     for i in range(0, days_to_test):
         if i==0:
-            test_datetime_from = datetime(*time.strptime(time_str, '%Y-%m-%d %H:%M:%S')[:6]) - timedelta(days=days_to_test)
+            test_datetime_from = time_str - timedelta(days=days_to_train) - timedelta(days=days_to_test)
         test_datetime_till = test_datetime_from + timedelta(days=1)
         test_datetime_till_clicks = test_datetime_till + timedelta(hours=2)
         views_test = cc.execute(clickhouse_query_template_views.format(', '.join(ch_fields), test_datetime_from, test_datetime_till))
